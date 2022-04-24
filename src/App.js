dc3ad71d15e5c8771ba82d7bc2e748e2619b0f66
anas110440 <75642207+anas110440@users.noreply.github.com>
2022-04-24 07:56:03 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   return (
 <div className="App">
 <BrowserRouter>
-<Home />
 <Routes>
 
 <Route path='/' element={<Home />} />
+<Route path='/petro-yem-med' element={<Home />} />
 
 
 <Route path='/home' element={<Home />} />
